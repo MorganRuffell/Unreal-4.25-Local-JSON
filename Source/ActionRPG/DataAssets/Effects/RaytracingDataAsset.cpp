ca6ab176f042de13cf5c7491c2fda07584a07d4b
Morgan Ruffell <morgan.ruffell@ICloud.com>
2021-04-30 17:00:06 +0100

    Debugging Gameplay loop

--- Source/ActionRPG/DataAssets/Effects/RaytracingDataAsset.cpp
+++ Source/ActionRPG/DataAssets/Effects/RaytracingDataAsset.cpp
@@ -15,10 +15,32 @@ URaytracingDataAsset::~URaytracingDataAsset()
 
 TSharedPtr<FJsonObject> URaytracingDataAsset::ToJson()
 {
-	return TSharedPtr<FJsonObject>();
+	TSharedPtr<FJsonObject> RaytracingData = MakeShared<FJsonObject>();
+
+	RaytracingData->SetNumberField("Intensity_AO", Intensity);
+	RaytracingData->SetNumberField("Radius_AO", Radius);
+	RaytracingData->SetNumberField("MaxRoughness", MaxRoughness);
+	RaytracingData->SetNumberField("RaytracingIntensity", RIntensity);
+
+	RaytracingData->SetBoolField("IsPostProcessEnabled", IsPostProcessEnabled);
+	RaytracingData->SetBoolField("IsAOEnabled", IsEnabled);
+	RaytracingData->SetBoolField("CanIncludeTranslucents", IncludeTranslucents);
+	RaytracingData->SetBoolField("IsAOEnabled", CanUseRefractions);
+
+	return RaytracingData;
 }
 
 bool URaytracingDataAsset::FromJson(FJsonObject& jsonObject)
 {
-	return false;
+	Intensity = jsonObject.GetNumberField("IntensityAO");
+	Radius = jsonObject.GetNumberField("Radius_AO");
+	MaxRoughness = jsonObject.GetNumberField("MaxRoughness");
+	RIntensity = jsonObject.GetNumberField("RaytracingIntensity");
+
+	IsPostProcessEnabled = jsonObject.GetBoolField("IsPostProcessEnabled");
+	IsEnabled = jsonObject.GetBoolField("IsAOEnabled");
+	IncludeTranslucents = jsonObject.GetBoolField("CanIncludeTranslucents");
+	CanUseRefractions = jsonObject.GetBoolField("IsAOEnabled");
+
+	return true;
 }
