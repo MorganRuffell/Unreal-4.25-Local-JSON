a3b262ec29970480dc5ba68271ea0b20446e2142
Morgan Ruffell <morgan.ruffell@ICloud.com>
2021-05-17 23:24:26 +0100

    Weird bug

---
+++ Source/ActionRPG/DataAssets/SequencerDataAsset.h
@@ -0,0 +1,26 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DataAssets/JSON/JSONDataAssetBase.h"
+#include "SequencerDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONRPG_API USequencerDataAsset : public UJSONDataAssetBase
+{
+	GENERATED_BODY()
+	
+public:
+
+	USequencerDataAsset();
+
+public:
+
+	
+
+
+};
